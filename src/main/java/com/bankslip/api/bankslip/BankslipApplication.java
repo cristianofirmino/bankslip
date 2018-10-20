2b7ff1e3c61636c6f6d2081deb23bfd2b77c0985
Cristiano Firmino <cristianofirmino@gmail.com>
2018-10-20 12:27:41 -0300

    Commit inicial of the project

---
+++ src/main/java/com/bankslip/api/bankslip/BankslipApplication.java
@@ -0,0 +1,12 @@
+package com.bankslip.api.bankslip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankslipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankslipApplication.class, args);
+	}
+}
